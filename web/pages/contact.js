8dd1b91310a2c19a1bdf3ca1d058e726f2ebe61b
Kyle Ferran <kyle.ferran@gmail.com>
2021-02-03 21:30:16 -0700

    add contacts table
    setup dynamodb table for contacts
    add aws dependencies

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -1,49 +1,43 @@
 import Layout from "../components/layout";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout title="Contact | RAVENS">
       <div className="prose text-center max-w-5xl mx-auto pt-12">
         <div className="py-12">
-          <h2 className="font-bold text-3xl">Contact</h2>
-          <p className="pt-8 mt-12">
-            Mascarpone cheeseburger cottage cheese. Roquefort smelly cheese
-            airedale cheese slices cheesy feet stilton rubber cheese cheesecake.
-            Cheddar cheesy grin cheddar blue castello cheese slices stinking
-            bishop cheesecake blue castello. Bocconcini smelly cheese cheese and
-            biscuits blue castello roquefort rubber cheese cottage cheese cheesy
-            feet. Port-salut taleggio fromage frais.
-          </p>
-          <p className="pt-8 mt-12">
-            Who moved my cheese jarlsberg ricotta. Everyone loves brie squirty
-            cheese cheese on toast st. agur blue cheese cheesecake caerphilly
-            roquefort. Cut the cheese emmental say cheese who moved my cheese
-            smelly cheese smelly cheese cheese strings taleggio. Chalk and
-            cheese cheese triangles cheese slices squirty cheese stilton danish
-            fontina everyone loves fondue. Roquefort bocconcini.
-          </p>
-          <p className="pt-8 mt-12">
-            Pecorino smelly cheese stinking bishop. Pecorino chalk and cheese
-            roquefort camembert de normandie red leicester fromage squirty
-            cheese cottage cheese. Pecorino brie cheesy grin rubber cheese
-            pecorino blue castello pepper jack port-salut. Brie caerphilly say
-            cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cheese and wine caerphilly fromage frais. Feta edam cheese on toast
-            hard cheese smelly cheese squirty cheese fondue camembert de
-            normandie. Cheesy grin fromage frais feta pepper jack cheesecake
-            jarlsberg everyone loves cream cheese. Gouda cottage cheese
-            cauliflower cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cauliflower cheese manchego hard cheese. Who moved my cheese rubber
-            cheese halloumi cottage cheese macaroni cheese roquefort boursin
-            melted cheese. Cut the cheese cheese slices goat monterey jack
-            taleggio caerphilly cheddar mascarpone. Gouda dolcelatte lancashire
-            fromage frais mascarpone.
-          </p>
+          <h1 className="inline-block px-4 lg:px-32 mx-auto pb-10 text-4xl text-center text-gold border-b-2 border-gold uppercase">Contact</h1>
+          <div className="relative mx-auto">
+            <div className="py-16 px-4">
+              <div className="max-w-lg mx-auto lg:max-w-none">
+                <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+                  <div>
+                    <label for="full_name" className="sr-only">Full name</label>
+                    <input type="text" name="full_name" id="full_name" autocomplete="name" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Full name"/> 
+                  </div>
+                  <div>
+                    <label for="email" className="sr-only">Email</label>
+                    <input id="email" name="email" type="email" autocomplete="email" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Email"/>
+                  </div>
+                  <div>
+                    <label for="phone" className="sr-only">Phone</label>
+                    <input type="text" name="phone" id="phone" autocomplete="tel" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Phone"/>
+                  </div>
+                  <div>
+                    <label for="message" className="sr-only">Message</label>
+                    <textarea id="message" name="message" rows="4" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Message"></textarea>
+                  </div>
+                  <div>
+                    <button type="submit" className="hidden lg:inline-block rounded-full font-bold uppercase tracking-wider border border-white py-2 px-8 hover:bg-gold hover:text-black transition-all">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+
         </div>
 
         <hr className="border-2 border-gold my-8" />
