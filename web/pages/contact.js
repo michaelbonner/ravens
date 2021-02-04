5f156b81ac381a341691d4f3ac18237f0d96f1dd
Kyle Ferran <kyle.ferran@gmail.com>
2021-02-03 22:41:10 -0700

    ui clean up, stub form submission

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -1,7 +1,47 @@
+import { useState } from 'react'
 import Layout from "../components/layout";
 // import Image from "next/image";
 
+/**
+ * @TODO
+ * validation
+ * ui error handling
+ * form submission notification
+ */
+
 export default function Home() {
+  const [name, setName] = useState("");
+  const [emailAddress, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          emailAddress: emailAddress,
+          phoneNumber: phoneNumber,
+          message: message
+        })
+      })
+
+      // if(response?.status === 200){
+
+      // }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Layout title="Contact | RAVENS">
       <div className="prose text-center max-w-5xl mx-auto pt-12">
@@ -10,22 +50,53 @@ export default function Home() {
           <div className="relative mx-auto">
             <div className="py-16 px-4">
               <div className="max-w-lg mx-auto lg:max-w-none">
-                <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+                <form 
+                  onSubmit={handleSubmit} 
+                  method="POST" 
+                  className="grid grid-cols-1 gap-y-6"
+                >
                   <div>
-                    <label for="full_name" className="sr-only">Full name</label>
-                    <input type="text" name="full_name" id="full_name" autocomplete="name" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Full name"/> 
+                    <label className="sr-only">Full name</label>
+                    <input 
+                      type="text" 
+                      name="name" 
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="block w-full shadow-sm py-3 px-4 text-gray-500 placeholder-gray-500  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" 
+                      placeholder="Full name"/> 
                   </div>
                   <div>
-                    <label for="email" className="sr-only">Email</label>
-                    <input id="email" name="email" type="email" autocomplete="email" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Email"/>
+                    <label className="sr-only">Email</label>
+                    <input 
+                      type="email" 
+                      autoComplete="email" 
+                      value={emailAddress}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="block w-full shadow-sm py-3 px-4 text-gray-500 placeholder-gray-500  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" 
+                      placeholder="Email"
+                    />
                   </div>
                   <div>
-                    <label for="phone" className="sr-only">Phone</label>
-                    <input type="text" name="phone" id="phone" autocomplete="tel" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Phone"/>
+                    <label className="sr-only">Phone</label>
+                    <input 
+                      type="text" 
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                      className="block w-full shadow-sm py-3 px-4 text-gray-500 placeholder-gray-500  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" 
+                      placeholder="Phone"
+                    />
                   </div>
                   <div>
-                    <label for="message" className="sr-only">Message</label>
-                    <textarea id="message" name="message" rows="4" className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Message"></textarea>
+                    <label className="sr-only">Message</label>
+                    <textarea 
+                      id="message" 
+                      name="message" 
+                      rows="4" 
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      className="block w-full shadow-sm py-3 px-4 text-gray-500 placeholder-gray-500  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" 
+                      placeholder="Message"
+                    ></textarea>
                   </div>
                   <div>
                     <button type="submit" className="hidden lg:inline-block rounded-full font-bold uppercase tracking-wider border border-white py-2 px-8 hover:bg-gold hover:text-black transition-all">
@@ -36,10 +107,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-
         </div>
-
         <hr className="border-2 border-gold my-8" />
       </div>
     </Layout>
