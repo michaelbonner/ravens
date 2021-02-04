a1fd12376fe9fceade8154a2e20f642afbf85bcd
Kyle Ferran <kyle.ferran@gmail.com>
2021-02-03 22:41:36 -0700

    stub out dynamodb put/insert

--- web/pages/api/contact.js
+++ web/pages/api/contact.js
@@ -1,3 +1,23 @@
+import * as uuid from 'uuid';
+import dynamoDb from '../../lib/dynamo-db';
+
 export default async function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+    if (req.method === 'POST') {
+        const contact = {
+          id: uuid.v4(),
+          name: req.body.name,
+          emailAddress: req.body.emailAddress,
+          phoneNumber: req.body.phoneNumber,
+          message: req.body.message,
+          createdAt: Date.now()
+        };
+    
+        await dynamoDb.put({
+          contact: contact
+        });
+    
+        res.status(201).json(contact);
+    }
+
+    // res.status(200).json({ name: 'John Doe' })
 }
