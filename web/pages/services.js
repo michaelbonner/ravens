3d1a0860349364f7f540fcbcb38454eac0e07022
Kyle Ferran <kyle.ferran@gmail.com>
2021-02-06 16:41:36 -0700

    stub out services page

--- web/pages/services.js
+++ web/pages/services.js
@@ -3,49 +3,69 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout title="Contact | RAVENS">
-      <div className="prose text-center max-w-5xl mx-auto pt-12">
+    <Layout title="Services | RAVENS">
+      <div className="prose text-center mx-auto pt-12">
         <div className="py-12">
-          <h2 className="font-bold text-3xl">Services</h2>
-          <p className="pt-8 mt-12">
-            Mascarpone cheeseburger cottage cheese. Roquefort smelly cheese
-            airedale cheese slices cheesy feet stilton rubber cheese cheesecake.
-            Cheddar cheesy grin cheddar blue castello cheese slices stinking
-            bishop cheesecake blue castello. Bocconcini smelly cheese cheese and
-            biscuits blue castello roquefort rubber cheese cottage cheese cheesy
-            feet. Port-salut taleggio fromage frais.
-          </p>
-          <p className="pt-8 mt-12">
-            Who moved my cheese jarlsberg ricotta. Everyone loves brie squirty
-            cheese cheese on toast st. agur blue cheese cheesecake caerphilly
-            roquefort. Cut the cheese emmental say cheese who moved my cheese
-            smelly cheese smelly cheese cheese strings taleggio. Chalk and
-            cheese cheese triangles cheese slices squirty cheese stilton danish
-            fontina everyone loves fondue. Roquefort bocconcini.
-          </p>
-          <p className="pt-8 mt-12">
-            Pecorino smelly cheese stinking bishop. Pecorino chalk and cheese
-            roquefort camembert de normandie red leicester fromage squirty
-            cheese cottage cheese. Pecorino brie cheesy grin rubber cheese
-            pecorino blue castello pepper jack port-salut. Brie caerphilly say
-            cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cheese and wine caerphilly fromage frais. Feta edam cheese on toast
-            hard cheese smelly cheese squirty cheese fondue camembert de
-            normandie. Cheesy grin fromage frais feta pepper jack cheesecake
-            jarlsberg everyone loves cream cheese. Gouda cottage cheese
-            cauliflower cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cauliflower cheese manchego hard cheese. Who moved my cheese rubber
-            cheese halloumi cottage cheese macaroni cheese roquefort boursin
-            melted cheese. Cut the cheese cheese slices goat monterey jack
-            taleggio caerphilly cheddar mascarpone. Gouda dolcelatte lancashire
-            fromage frais mascarpone.
-          </p>
-        </div>
+          <h1 className="inline-block px-4 lg:px-32 mx-auto pb-10 text-4xl text-center text-gold border-b-2 border-gold uppercase">Services</h1>
 
+          <section className="pt-8 mt-12">
+            <header class="relative h-screen mb-12">
+              <Image 
+                src="/images/services-aerial.jpg"
+                alt="Aerial Cinema"
+                layout="fill"
+              />              
+            </header>
+            <div>
+              <h2 className="font-bold text-3xl uppercase">Aerial Cinema</h2>
+              <p className="py-4">The tougher and more technical the shot, the better.</p>
+              <p className="py-4">We offer a variety of platforms for any aerial need or budget. We have a well-oiled team with the experience and taste to get you the shot you want or better. Ready to travel anywhere in the world for production, but local to the beautiful vistas of Utah. See some of the options we provide:</p>
+              <ul className="">
+                <li>HEAVY LIFT DRONE PLATFORM - FREEFLY ALTA X</li>
+                <li>MID-SIZE DRONE PLATFORM - FREEFLY ASTRO</li>
+                <li>SMALL DRONE PLATFORM - DJI MAVIC 2 PRO</li>
+                <li>FPV CINEMA DRONE PLATFORM - SICARIO W/ RED KOMODO</li>
+                <li>HELICOPTER AERIALS - CUSTOM PLATFORMS</li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="pt-8 mt-12">
+            <header class="relative h-screen mb-12">
+              <Image 
+                src="/images/services-pursuit-tracking.jpg"
+                alt="Aerial Cinema"
+                layout="fill"
+              />              
+            </header>
+            <div>
+              <h2 className="font-bold text-3xl uppercase">PURSUIT TRACKING</h2>
+              <p className="py-4">Laying it all on the line, in the pursuit of glory.</p>
+              <p className="py-4">We offer a variety of platforms for any pursuit need or budget. We have a well-oiled team with the experience and taste to get you the shot you want or better. Ready to travel anywhere in the world for production, but local to the beautiful vistas of Utah. See some of the options we provide:</p>
+              <ul className="">
+                <li>AWD STREET & OFF-ROAD TRACKING - LAND ROVER RIG</li>
+                <li>RUGGED/NARROW OFF-ROAD TRACKING - CAN-AM 4X4</li>
+                <li>RUSSIAN ARM TRACKING - COMING SOON</li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="pt-8 mt-12">
+            <header class="relative h-screen mb-12">
+              <Image 
+                src="/images/services-super-slow-motion.jpg"
+                alt="Aerial Cinema"
+                layout="fill"
+              />              
+            </header>
+            <div>
+              <h2 className="font-bold text-3xl uppercase">SUPER SLOW-MOTION</h2>
+              <p className="py-4">Revealing unseen wonders through high-speed cinematography.</p>
+              <p className="py-4">We are experts at maximizing the Phantom Flex 4K platform to capture the secrets of movement <br/> hidden to the human eye. Tone and texture are a specialty. Story is our foundation.</p>
+            </div>
+          </section>
+          
+        </div>
         <hr className="border-2 border-gold my-8" />
       </div>
     </Layout>
