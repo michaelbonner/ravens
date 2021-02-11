3377245013e74217e8b8c20dafedda1271ab8acb
Kyle Ferran <kyle.ferran@gmail.com>
2021-02-10 20:56:13 -0700

    add equipment markup

--- web/pages/equipment.js
+++ web/pages/equipment.js
@@ -3,50 +3,116 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout title="Contact | RAVENS">
-      <div className="prose text-center max-w-5xl mx-auto pt-12">
-        <div className="py-12">
-          <h2 className="font-bold text-3xl">Equipment</h2>
-          <p className="pt-8 mt-12">
-            Mascarpone cheeseburger cottage cheese. Roquefort smelly cheese
-            airedale cheese slices cheesy feet stilton rubber cheese cheesecake.
-            Cheddar cheesy grin cheddar blue castello cheese slices stinking
-            bishop cheesecake blue castello. Bocconcini smelly cheese cheese and
-            biscuits blue castello roquefort rubber cheese cottage cheese cheesy
-            feet. Port-salut taleggio fromage frais.
-          </p>
-          <p className="pt-8 mt-12">
-            Who moved my cheese jarlsberg ricotta. Everyone loves brie squirty
-            cheese cheese on toast st. agur blue cheese cheesecake caerphilly
-            roquefort. Cut the cheese emmental say cheese who moved my cheese
-            smelly cheese smelly cheese cheese strings taleggio. Chalk and
-            cheese cheese triangles cheese slices squirty cheese stilton danish
-            fontina everyone loves fondue. Roquefort bocconcini.
-          </p>
-          <p className="pt-8 mt-12">
-            Pecorino smelly cheese stinking bishop. Pecorino chalk and cheese
-            roquefort camembert de normandie red leicester fromage squirty
-            cheese cottage cheese. Pecorino brie cheesy grin rubber cheese
-            pecorino blue castello pepper jack port-salut. Brie caerphilly say
-            cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cheese and wine caerphilly fromage frais. Feta edam cheese on toast
-            hard cheese smelly cheese squirty cheese fondue camembert de
-            normandie. Cheesy grin fromage frais feta pepper jack cheesecake
-            jarlsberg everyone loves cream cheese. Gouda cottage cheese
-            cauliflower cheese.
-          </p>
-          <p className="pt-8 mt-12">
-            Cauliflower cheese manchego hard cheese. Who moved my cheese rubber
-            cheese halloumi cottage cheese macaroni cheese roquefort boursin
-            melted cheese. Cut the cheese cheese slices goat monterey jack
-            taleggio caerphilly cheddar mascarpone. Gouda dolcelatte lancashire
-            fromage frais mascarpone.
-          </p>
+    <Layout title="Equipment | RAVENS">
+      <div className="prose text-center max-w-5xl mx-auto">
+        <div >
+          <h1 className="inline-block px-4 lg:px-32 mx-auto pb-10 text-4xl text-center text-gold border-b-2 border-gold uppercase">Equipment</h1>
+          <section className="pt-8 mt-12">
+            <header className="relative mb-12">
+              <Image 
+                src="/images/altax-groundview.png"
+                alt="FREEFLY ALTA X // AERIAL PLATFORM"
+                layout="intrinsic"
+                width={450}
+                height={300}
+              />
+              <h2 className="font-bold mt-12 mb-3 text-2xl uppercase">FREEFLY ALTA X // AERIAL PLATFORM</h2>
+              <p className="py-4 text-sm">Designed to get cinema camera packages into the air, the Freefly Alta X is the king of the roost. With a top speed of 60 mph, 10 min flight time, and camera configurable on both top and bottom, this bird is on the cutting edge of aerial cinema technology.</p>
+              <p className="py-4 text-sm">Check out the more specific payload specs below:</p>
+            </header>
+          
+            <div className="flex flex-col">
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                  <div className="overflow-hidden">
+                    <table className="min-w-full table-auto md:table-fixed mb-8">
+                      <thead>
+                        <tr className="border-b">
+                          <th className="text-sm pb-4 text-left">Platform</th>
+                          <th className="text-sm pb-4">Weight</th>
+                          <th className="text-sm pb-4">REM Payload <br/>(55 lb Limit)</th>
+                          <th className="text-sm pb-4">EXT Payload <br/>(63 lb)</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td  className="pb-4 pt-4 text-xs text-left">ALTA X W/ Batteries</td>
+                          <td  className="pb-4 pt-4 text-xs">38.39 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">16.61 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">24.61 lbs</td>
+                        </tr>
+                        <tr>
+                          <td  className="pb-4 pt-4 text-xs text-left">MOVI PRO GIMBAL</td>
+                          <td  className="pb-4 pt-4 text-xs">5.84 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">10.77 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">18.77 lbs</td>
+                        </tr>
+                        <tr>
+                          <td  className="pb-4 pt-4 text-xs text-left">RONIN 2 GIMBAL</td>
+                          <td  className="pb-4 pt-4 text-xs">11 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">5.61 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">13.61 lbs</td>
+                        </tr>
+                        <tr>
+                          <td  className="pb-4 pt-4 text-xs text-left">ALTA X W/ Batteries</td>
+                          <td  className="pb-4 pt-4 text-xs">38.39 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">16.61 lbs</td>
+                          <td  className="pb-4 pt-4 text-xs">24.61 lbs</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="md:px-28">
+              <p className="text-left text-sm pb-6"><span className="font-bold">REM PAYLOAD:</span> The remaining available payload for additional equipment (lenses, media, FIZ’s, etc) within FAA guidelines.</p>
+              <p className="text-left text-sm"><span className="font-bold">EXT PAYLOAD:</span> The remaining available payload for additional equipment (lenses, media, FIZ’s, etc) based on platform capability during Summer days in the SLC valley (90 F | 5000 ft elevation).</p>
+            </div>
+            <hr className="border-b-1 w-1/2 m-auto mt-16 border-gold"/>
+          </section>
+
+
+          <section className="pt-8 mb-12">
+            <header className="relative mb-12">
+              <Image 
+                src="/images/land-rover-discovery-front.png"
+                alt="Land Rover // Vehicle Tracking"
+                layout="intrinsic"
+                width={450}
+                height={300}
+              />
+            </header>
+
+            <div className="mt-12">
+              <h2 className="font-bold mb-3 text-2xl uppercase">LAND ROVER // VEHICLE TRACKING</h2>
+              <p className="py-4 text-sm">This sporty little beast makes brutally difficult tracking shots look effortless. With a top speed of 90 mph, 250 mile range, and camera configurable on both front & rear, this vehicle unlocks the possibilities.</p>
+            </div>
+
+            <hr className="border-b-1 w-1/2 m-auto mt-16 border-gold"/>
+          </section>
+
+          <section className="pt-8 mt-12">
+            <header className="relative mb-12">
+              <Image 
+                src="/images/phantom-flex-4k.jpg"
+                alt="PHANTOM FLEX 4K // HIGH-SPEED"
+                layout="intrinsic"
+                width={450}
+                height={300}
+              />     
+            </header>
+            
+            <div className="mt-12">
+              <h2 className=" mb-3 font-bold text-2xl uppercase">PHANTOM FLEX 4K // HIGH-SPEED</h2>
+              <p className="py-4 text-sm">Nothing beats the Phantom Flex 4K for quality in the 1200 fps range. Made for specialists, this camera system requires an operator to make full use of it’s potential.</p>
+            </div>
+
+            <hr className="border-b-1 w-1/2 m-auto mt-16 border-gold"/>
+          </section>
         </div>
 
-        <hr className="border-2 border-gold my-8" />
       </div>
     </Layout>
   );
