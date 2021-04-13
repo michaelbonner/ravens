efb6531074686fb9732ba1fb823e04e6e5c64ce7
Michael Bonner <michaelwbonner@gmail.com>
2021-04-13 13:15:27 -0600

    fix email in contact submission

--- web/pages/api/contact.js
+++ web/pages/api/contact.js
@@ -20,11 +20,11 @@ export default async function handler(req, res) {
         `noreply@ravens.works`,
         `New Ravens contact submission from ${req.body.name}`,
         `<p>Name: ${req.body.name}</p>
-        <p>Email: ${req.body.email}</p>
+        <p>Email: ${req.body.emailAddress}</p>
         <p>Phone: ${req.body.phoneNumber}</p>
         <p>Message: ${req.body.message}</p>`,
         `Name: ${req.body.name}\n
-        Email: ${req.body.email}\n
+        Email: ${req.body.emailAddress}\n
         Phone: ${req.body.phoneNumber}\n
         Message: ${req.body.message}`
       );
