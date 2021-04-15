8e32096503036342ed3b5b1f8b55c0cb0f2a3bea
Michael Bonner <michaelwbonner@gmail.com>
2021-04-15 12:36:00 -0600

    adjust aspect ratio stuff on home video

--- web/components/layout.js
+++ web/components/layout.js
@@ -62,7 +62,6 @@ const Layout = ({
     if (heroVideoUrl) {
       setHeroStyles({
         position: "relative",
-        width: "100%",
         top: 0,
         right: 0,
         bottom: 0,
@@ -95,7 +94,14 @@ const Layout = ({
               allow="autoplay; fullscreen; picture-in-picture"
               allowFullScreen={true}
               title="Ravens Film Works"
-              className="absolute inset-0 w-full h-full"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                pointerEvents: "none",
+              }}
             ></iframe>
           </div>
         )}
