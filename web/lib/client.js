c03060f4b48429a511cbb48855181ffde95a7da1
Michael Bonner <mbp132@michaels-mbp.lan>
2021-04-21 21:24:32 -0600

    add api version to sanity client

--- web/lib/client.js
+++ web/lib/client.js
@@ -4,4 +4,5 @@ export default sanityClient({
   projectId: "nnjdfopi", // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: "2021-03-25",
 });
