b5d3dcdb72a162a4693a3a3abe6c3b6f2e97b44a
Michael Bonner <michaelwbonner@gmail.com>
2021-07-01 18:59:29 -0600

    add missing contact schema

---
+++ studio/schemas/contact.js
@@ -0,0 +1,28 @@
+import { MdContacts as icon } from 'react-icons/md'
+
+export default {
+  name: 'contact',
+  title: 'Contact',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'poster',
+      title: 'Poster Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+}
