5a62b03fb015fcfad80cd728b95dc47d7bf7ab6b
Michael Bonner <michaelwbonner@gmail.com>
2021-08-16 17:50:36 -0600

    best practice updates

--- web/pages/index.js
+++ web/pages/index.js
@@ -49,9 +49,9 @@ const Home = (props) => {
                     index + 1 !== services.length ? `border-b-2` : ``
                   } lg:border-2 border-gold hover:border-gray-500 transition-all ease-in duration-300 bg-gradient-to-t from-transparent to-transparent hover:to-gray-700`}
                 >
-                  <h3 className="text-3xl font-bold lg:px-10">
+                  <h2 className="text-3xl font-bold lg:px-10">
                     {service.title}
-                  </h3>
+                  </h2>
                   <div className="flex flex-1 items-center my-8">
                     <div className="h-38 w-3/4 mx-auto">
                       <img
