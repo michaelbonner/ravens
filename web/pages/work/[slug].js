dcdb3f7fda6c5315c81f9c04fcfb63e2c9fff415
Michael Bonner <michaelwbonner@gmail.com>
2021-08-17 14:29:39 -0600

    prevent purging of aspect ratio classes

--- web/pages/work/[slug].js
+++ web/pages/work/[slug].js
@@ -32,6 +32,26 @@ const projectQuery = groq`
 }
 `;
 
+/*
+prevent purging of aspect ratio
+aspect-w-1	aspect-h-1
+aspect-w-2	aspect-h-2
+aspect-w-3	aspect-h-3
+aspect-w-4	aspect-h-4
+aspect-w-5	aspect-h-5
+aspect-w-6	aspect-h-6
+aspect-w-7	aspect-h-7
+aspect-w-8	aspect-h-8
+aspect-w-9	aspect-h-9
+aspect-w-10	aspect-h-10
+aspect-w-11	aspect-h-11
+aspect-w-12	aspect-h-12
+aspect-w-13	aspect-h-13
+aspect-w-14	aspect-h-14
+aspect-w-15	aspect-h-15
+aspect-w-16	aspect-h-16
+*/
+
 const Project = ({ project = {}, projects = [] }) => {
   const [showVideo, setShowVideo] = useState(false);
   const router = useRouter();
