e0d029c193a487be9a5b45dcb6667de67e9759a3
Michael Bonner <michaelwbonner@gmail.com>
2023-10-02 11:35:31 -0600

    ignore /obfuscated in sitemap

--- web/next-sitemap.js
+++ web/next-sitemap.js
@@ -1,4 +1,20 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://ravensfilmworks.com",
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    // custom function to ignore the path
+    console.log("path", path);
+    if (path === "/obfuscated") {
+      return null;
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
