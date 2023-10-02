015777bcead4fcf8d7f97265bd6f5e54f2436529
Michael Bonner <michaelwbonner@gmail.com>
2023-10-02 11:29:41 -0600

    add obfuscated page

---
+++ web/pages/obfuscated.js
@@ -0,0 +1,19 @@
+import Layout from "../components/layout";
+
+const Obfuscated = () => {
+  return (
+    <Layout title="Nothing | RAVENS">
+      <div className="container mx-auto text-center">
+        <h1 className="inline-block px-4 lg:px-32 mx-auto pb-10 text-4xl text-center text-gold border-b-2 border-gold uppercase mb-12">
+          Nothing here for you
+        </h1>
+        <p>
+          You found a page we have set up just for bots. If you are a human, use
+          the navigation to find the page you&apos;re looking for.
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Obfuscated;
